6978f082634ca309b1af271c88e2e26387599e4e
zunlu <1830926886@qq.com>
2017-10-31 22:48:28 +0800

    tokens

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var _config = {
     owner           : 'zunlu',           // github 用户名
     repo            : 'blog',// github 中对应项目名
     duoshuo_id      : 'absurd',            // 在第三方评论插件多说申请站点的子域名
-    access_token : 'aabe385f021dfb3714add63f82bec6a104177bd1',                     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    access_token : '1f71ac72312a8bbb41a281fd46949a9cff7064ee',                     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     per_page        : '15'                    // 默认一页显示几篇文章
 }
 
